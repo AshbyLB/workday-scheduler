da17ca36fefdc19e9798296a6f5198ff9dd89b89
Ashby Blakely <ashbyleeblakely@gmail.com>
2021-08-26 16:18:27 -0400

    Added grid to the html and added date to the top of the page

---
+++ js/script.js
@@ -0,0 +1,20 @@
+var container = $(".container");
+
+var save = window.localStorage;
+
+var timeNow = moment().format("hA");
+
+$("#currentDay").text(moment().format("dddd MMMM DD, YYYY"));
+
+/*var daySpan = [];
+for(var i=9; i < 18; i++) {
+    daySpan.push((i < 10 ? "0" : "") + i + ":00");
+    console.log(daySpan);
+}*/
+
+var colorChange;
+
+
+
+
+

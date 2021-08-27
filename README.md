f564d2ce72619ec7052df8e72e4059c90e67d199
Ashby Blakely <ashbyleeblakely@gmail.com>
2021-08-27 17:56:20 -0400

    Added readme and the function to call the data back from local storage

--- README.md
+++ README.md
@@ -1,2 +1,23 @@
-# workday-scheduler
-A planner for your workday. 
+# Hourly Work Planner 
+ 
+[Link to LIVE site.]()
+
+## Table of Contents
+- [Description](#Description)
+- [Usage](#Usage)
+- [Screenshot](#Screenshot)
+- [Languages](#Languages)
+
+## Discription
+Hello.  Thanks you for using the Hourly Work Planner! Feel free to input your plans for the day and rest assured they will be saved until you decide to change them.
+
+## Usage 
+To navigate the page, click on the text area next to the hour you need to add plans.  Add your plans into the text field and then click the save button to the right!  That is all you have to do and the browser will save your plans until you change them again!
+
+## Screenshot
+ 
+
+![me]()
+
+## Languages
+HTML, CSS, JavaScript, JQuery, Bootstrap, and FontAwesome 
--- js/script.js
+++ js/script.js
@@ -1,9 +1,10 @@
 var container = $(".container");
 var textInput = $("textarea");
 var saveBtn = $(".saveBtn");
-var save = window.localStorage;
 var timeNow = moment().format("H");
 var timeB = $('.time-block');
+
+savedPl();
 
 $("#currentDay").text(moment().format("dddd MMMM DD, YYYY"));
 
@@ -27,11 +28,23 @@ saveBtn.on("click", function(event){
     var inputUser = $(this).siblings(".description").val();
 
     localStorage.setItem(inputTime, inputUser);
+
 });
 
-function savedPlans(){
 
+
+function savedPl(){
+$("#9 .description").val(localStorage.getItem("9"));
+$("#10 .description").val(localStorage.getItem("10"));
+$("#11 .description").val(localStorage.getItem("11"));
+$("#12 .description").val(localStorage.getItem("12"));
+$("#13 .description").val(localStorage.getItem("13"));
+$("#14 .description").val(localStorage.getItem("14"));
+$("#15 .description").val(localStorage.getItem("15"));
+$("#16 .description").val(localStorage.getItem("16"));
+$("#17 .description").val(localStorage.getItem("17"));
 }
+
 
 
 

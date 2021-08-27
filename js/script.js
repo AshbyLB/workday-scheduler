67bb9af4e40d5da8288d3e8dcc288b6e3f33a762
Ashby Blakely <ashbyleeblakely@gmail.com>
2021-08-26 21:01:32 -0400

    updated the JS to have the timeB statement

--- js/script.js
+++ js/script.js
@@ -2,19 +2,37 @@ var container = $(".container");
 
 var save = window.localStorage;
 
-var timeNow = moment().format("hA");
+//var timeNow = moment().format("H");
+
+var timeNow = "11";
+
+var timeB = $('.time-block');
 
 $("#currentDay").text(moment().format("dddd MMMM DD, YYYY"));
 
-/*var daySpan = [];
-for(var i=9; i < 18; i++) {
-    daySpan.push((i < 10 ? "0" : "") + i + ":00");
-    console.log(daySpan);
-}*/
 
-var colorChange;
+timeB.each(function() {
+    console.log(parseInt($(this).attr("id")));
+    console.log(parseInt(timeNow));
+    if (parseInt(timeNow) === parseInt($(this).attr("id"))) {
+        $(this).children("textarea").addClass("present");
+    }
+    else if (parseInt($(this).attr("id")) < parseInt(timeNow)) {
+        $(this).children("textarea").addClass("past");
+    }
+    else {
+        $(this).children("textarea").addClass("future");
+    }
+});
 
 
 
 
 
+$("textarea").on("click", function(){
+    console.log($(this).parent().attr("id"));
+})
+
+console.log(timeB.children("textarea"));
+
+console.log(timeB.attr("id"));
